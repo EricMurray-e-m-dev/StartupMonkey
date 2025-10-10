6e07cd070b66f3ec3d0ad317df6296d715ad1ef9
e-m-dev <eric.murray.dev@gmail.com>
2025-10-10 23:09:03 +0100

    Implement PostGresAdapter structure and method stubs for database interaction

--- collector/internal/adapter/postgres.go
+++ collector/internal/adapter/postgres.go
@@ -1,1 +1,26 @@
 package adapter
+
+import (
+	"database/sql"
+)
+
+type PostGresAdapter struct {
+	connectionString string
+	conn             *sql.DB
+}
+
+func (p *PostGresAdapter) Connect() (_ error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (p *PostGresAdapter) CollectMetrics() (_ *RawMetrics, _ error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (p *PostGresAdapter) Close() (_ error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (p *PostGresAdapter) HealthCheck() (_ error) {
+	panic("not implemented") // TODO: Implement
+}
