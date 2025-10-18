08d216294550c09e5c02c983026ddc84016fcbc0
e-m-dev <eric.murray.dev@gmail.com>
2025-10-18 11:37:38 +0100

    change test environment to overwrite exposed ports for github actions

--- tests/integration/framework/environment.go
+++ tests/integration/framework/environment.go
@@ -7,22 +7,23 @@ import (
 )
 
 type TestEnvironment struct {
-	t           *testing.T
-	ComposeFile string
-	ProjectName string
-	Services    []string
-	StartTime   time.Time
+	t               *testing.T
+	ComposeFile     string
+	ComposeTestFile string
+	ProjectName     string
+	Services        []string
+	StartTime       time.Time
 }
 
 func NewTestEnvironment(t *testing.T, services []string) *TestEnvironment {
-	// Use timestamp or random suffix to avoid conflicts
 	projectName := fmt.Sprintf("startupmonkey-test-%d", time.Now().Unix())
 
 	return &TestEnvironment{
-		t:           t,
-		ComposeFile: "../../docker-compose.yml",
-		ProjectName: projectName,
-		Services:    services,
-		StartTime:   time.Now(),
+		t:               t,
+		ComposeFile:     "../../docker-compose.yml",
+		ComposeTestFile: "../../docker-compose.test.yml",
+		ProjectName:     projectName,
+		Services:        services,
+		StartTime:       time.Now(),
 	}
 }
