04f12768aa962ee52e0e3024e0d15fdc478a35eb
e-m-dev <eric.murray.dev@gmail.com>
2025-10-21 19:30:39 +0100

    moved normaliser outside of internal

---
+++ analyser/internal/detector/detector.go
@@ -0,0 +1,14 @@
+package detector
+
+import (
+	"github.com/EricMurray-e-m-dev/StartupMonkey/analyser/internal/models"
+	"github.com/EricMurray-e-m-dev/StartupMonkey/collector/normaliser"
+)
+
+type Detector interface {
+	Detect(snapshot *normaliser.NormalisedMetrics) *models.Detection
+
+	Name() string
+
+	Category() models.DetectionCategory
+}
