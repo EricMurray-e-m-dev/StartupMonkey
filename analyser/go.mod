802e113bdcf4f1963d1fcc129e522f5cc241e425
e-m-dev <eric.murray.dev@gmail.com>
2025-10-28 20:20:59 +0000

    feat: implement NATS publisher for detection events

--- analyser/go.mod
+++ analyser/go.mod
@@ -5,6 +5,7 @@ go 1.25.1
 require (
 	github.com/EricMurray-e-m-dev/StartupMonkey/collector v0.0.0-20251021183526-f92fadc6882b
 	github.com/EricMurray-e-m-dev/StartupMonkey/proto v0.0.0-20251019185316-8f88e955d50e
+	github.com/nats-io/nats.go v1.47.0
 	github.com/stretchr/testify v1.11.1
 	google.golang.org/grpc v1.76.0
 )
@@ -15,6 +16,9 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.6 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/nats-io/nkeys v0.4.11 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.40.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
--- analyser/internal/eventbus/publisher.go
+++ analyser/internal/eventbus/publisher.go
@@ -1,1 +1,59 @@
 package eventbus
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/EricMurray-e-m-dev/StartupMonkey/analyser/internal/models"
+	"github.com/nats-io/nats.go"
+)
+
+type Publisher struct {
+	conn *nats.Conn
+}
+
+func NewPublisher(natsURL string) (*Publisher, error) {
+	conn, err := nats.Connect(natsURL,
+		nats.RetryOnFailedConnect(true),
+		nats.MaxReconnects(10),
+		nats.ReconnectWait(2*time.Second),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Analyser connected to NATS at %s", natsURL)
+
+	return &Publisher{
+		conn: conn,
+	}, nil
+}
+
+func (p *Publisher) PublishDetection(detection *models.Detection) error {
+
+	data, err := json.Marshal(detection)
+	if err != nil {
+		return err
+	}
+
+	if err := p.conn.Publish("detections", data); err != nil {
+		return err
+	}
+
+	log.Printf("Published detection to event bus: [%s] %s", detection.Severity, detection.Title)
+
+	return nil
+}
+
+func (p *Publisher) Close() {
+	if p.conn != nil {
+		p.conn.Close()
+		log.Printf("Analyser disconnected from NATS")
+	}
+}
+
+func (p *Publisher) IsConnected() bool {
+	return p.conn != nil && p.conn.IsConnected()
+}
